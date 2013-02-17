48c966ef4e786388dc08d09cb41f002fd763b18d
Benjamin Summers <bmsummers@ymail.com>
2013-02-17 17:58:39 -0500

    Login Panel
    Created a login Panel for the front end. All work now needs to be done
    in the back end.

---
+++ Twitter4j/src/gui/AuthenticatePanel.java
@@ -0,0 +1,121 @@
+package gui;
+
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+import twitter4j.TwitterException;
+
+import engine.TwitterEngine;
+/**
+ * AuthenticationPanel.class
+ * 
+ * Constructs the authentication panel which is palced
+ * inside of the west Panel.
+ * @author Ben
+ *
+ */
+@SuppressWarnings("serial")
+public class AuthenticatePanel extends JPanel{
+
+	private JPanel authPanel;
+	private JLabel userNameL, consumerKeyL, consumerSecretL,
+		acessTokenL, acessTokenSecretL;
+	private JTextField userNameF, consumerKeyF,
+		consumerSecretF, acessTokenF,
+		acessTokenSecretF;
+	private JButton authenticate, cancel;
+	
+	private String userName, consumerKey, consumerSecret,
+			accessToken, accessTokenS;
+	
+	private TwitterEngine engine;
+	
+	/**
+	 * Creates the Authenication GUI
+	 * @param engine 
+	 */
+	public AuthenticatePanel(TwitterEngine engine){
+		this.engine = engine;
+		
+		GridLayout authLayout = new GridLayout
+				(6, 2, 2, 5);
+		authPanel = new JPanel();
+		authPanel.setLayout(authLayout);
+		//username
+		userNameL = new JLabel("User Name: ");
+		userNameF = new JTextField(20);
+		authPanel.add(userNameL);
+		authPanel.add(userNameF);
+		//consumer key
+		consumerKeyL = new JLabel("Consumer Key: ");
+		consumerKeyF = new JTextField(20);
+		authPanel.add(consumerKeyL);
+		authPanel.add(consumerKeyF);
+		//consumer secret
+		consumerSecretL = new JLabel("Consumer Secret: ");
+		consumerSecretF = new JTextField(20);
+		authPanel.add(consumerSecretL);
+		authPanel.add(consumerSecretF);
+		//access token
+		acessTokenL = new JLabel("Acess Token: ");
+		acessTokenF = new JTextField(20);
+		authPanel.add(acessTokenL);
+		authPanel.add(acessTokenF);
+		//access token secret
+		acessTokenSecretL = new JLabel
+				("Acess Token Secret: ");
+		acessTokenSecretF = new JTextField(20);
+		authPanel.add(acessTokenSecretL);
+		authPanel.add(acessTokenSecretF);
+		//authenticate , cancel
+		authenticate = new JButton("Authenticate");
+		cancel = new JButton("Cancel");
+		authenticate.addActionListener(loginHandeler);
+		cancel.addActionListener(loginHandeler);
+		authPanel.add(authenticate);
+		authPanel.add(cancel);
+		add(authPanel);
+	}
+	/**
+	 * login Handeler manages the two buttons that are
+	 * present within the Authenticate Panel. Sends Data
+	 * to the Engine and error checks here.
+	 */
+	private ActionListener loginHandeler = new 
+			ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getActionCommand().equals("Authenticate")){
+				userName = userNameF.getText();
+				consumerKey = consumerKeyF.getText();
+				consumerSecret = consumerSecretF.getText();
+				accessToken = acessTokenF.getText();
+				accessTokenS = acessTokenSecretF.getText();
+				//if any thing is empty then the program
+				//can't work.
+				if(userName.length() == 0 ||
+						consumerKey.length() == 0 ||
+						consumerSecret.length() == 0 ||
+						accessToken.length() == 0 ||
+						accessTokenS.length() == 0){
+					JOptionPane.showMessageDialog(null, 
+							"Fill in all fields");
+				}else{
+					engine.login(userName,
+							consumerKey, consumerSecret,
+							accessToken, accessTokenS);
+					
+				}
+			}
+			if(e.getActionCommand().equals("Cancel"))
+				userNameF.setText("");
+				consumerKeyF.setText("");
+				consumerSecretF.setText("");
+				acessTokenF.setText("");
+				acessTokenSecretF.setText("");
+		}	
+	};
+}
--- Twitter4j/src/gui/TwitterGUI.java
+++ Twitter4j/src/gui/TwitterGUI.java
@@ -37,7 +37,7 @@ public class TwitterGUI extends javax.swing.JFrame{
 			fileDeleteStatus, generateWordFrequencyList, 
 			generateTopTrendingList, help_About;
 	private TwitterResultsPanel results;
-//	private AuthenticatePanel authP;
+	private AuthenticatePanel authP;
 //	private GetStatusPanel getStatusP;
 //	private GetTimePanel getTimeP;
 //	private PostPanel postP;
@@ -79,33 +79,24 @@ public class TwitterGUI extends javax.swing.JFrame{
 		westPanel = new JPanel();
 		westPanel.setLayout(new BoxLayout
 				(westPanel, BoxLayout.Y_AXIS));
-		JTabbedPane tabbedPane = new JTabbedPane();
-		JComponent panel1 = makeTextPanel("Panel #1");
-        tabbedPane.addTab("Tab 1", panel1);
-    //    tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
-         
-        JComponent panel2 = makeTextPanel("Panel #2");
-        tabbedPane.addTab("Tab 2",panel2);
-      //  tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
-         
-        JComponent panel3 = makeTextPanel("Panel #3");
-        tabbedPane.addTab("Tab 3", panel3);
-       // tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
-         
-        JComponent panel4 = makeTextPanel(
-                "Panel #4 (has a preferred size of 410 x 50).");
-        panel4.setPreferredSize(new Dimension(410, 50));
-        tabbedPane.addTab("Tab 4",panel4);
-       // tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
-        
+//		JTabbedPane tabbedPane = new JTabbedPane();
+//		JComponent panel1 = makeTextPanel("Panel #1");
+//        tabbedPane.addTab("Tab 1", panel1);
+//        JComponent panel2 = makeTextPanel("Panel #2");
+//        tabbedPane.addTab("Tab 2",panel2);
+//        JComponent panel3 = makeTextPanel("Panel #3");
+//        tabbedPane.addTab("Tab 3", panel3);
+//        JComponent panel4 = makeTextPanel(
+//                "Panel #4 (has a preferred size of 410 x 50).");
+//        tabbedPane.addTab("Tab 4",panel4);        
 		//Add the tabbed pane to this panel.
-		westPanel.add(tabbedPane);
+//		westPanel.add(tabbedPane);
         
 		//The following line enables to use scrolling tabs.
-		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+//		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
 //		combo.addActionListener(switchHandeler);
 		
-//		authP = new AuthenticatePanel(engine);
+		authP = new AuthenticatePanel(engine);
 //		getStatusP = new GetStatusPanel(engine);
 //		getTimeP = new GetTimePanel(engine);
 //		postP = new PostPanel(engine);
@@ -115,7 +106,7 @@ public class TwitterGUI extends javax.swing.JFrame{
         		("Select Operation");
 
 		westPanel.add(Box.createVerticalGlue());
-//		westPanel.add(authP);
+		westPanel.add(authP);
 		GUI.add(westPanel, BorderLayout.WEST);
 	}
 	
@@ -123,7 +114,7 @@ public class TwitterGUI extends javax.swing.JFrame{
         JPanel panel = new JPanel(false);
         JLabel filler = new JLabel(text);
         filler.setHorizontalAlignment(JLabel.CENTER);
-        panel.setLayout(new GridLayout(1, 1));
+        //panel.setLayout(new GridLayout(1, 1));
         panel.add(filler);
         return panel;
     }
